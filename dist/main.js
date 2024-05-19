988c2f3f49ee4481c109171500358dcee1c8f08e
tharsikan1999 <tharsikanking@gmail.com>
2024-05-19 22:06:35 +0530

    config cross origin

--- dist/main.js
+++ dist/main.js
@@ -5,6 +5,12 @@ const app_module_1 = require("./app.module");
 const common_1 = require("@nestjs/common");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.enableCors({
+        origin: "http://localhost:3001",
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+        allowedHeaders: "Content-Type, Accept",
+        credentials: true,
+    });
     app.useGlobalPipes(new common_1.ValidationPipe({
         whitelist: true,
         transform: true,
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,15 @@ import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS
+  app.enableCors({
+    origin: "http://localhost:3001", // specify allowed origin(s)
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    allowedHeaders: "Content-Type, Accept",
+    credentials: true, // if you need to allow cookies
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
